75cbd50fe440ac41de5bf4f3ddb7d225352e8a5a
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2014-04-18 22:45:54 +0700

    Bavensky

--- _74HC595/_74HC595.ino
+++ _74HC595/_74HC595.ino
@@ -1,4 +1,12 @@
-// IC Shift Register 74HC595
+/*******************************************************************************
+ * Project  : IC Shift Register 74HC595                                        *
+ * Compiler : Arduino 1.5.2                                                    *
+ * Board    : Arduino DUE                                                      *
+ * Device   : 74HC595 LED                                                      *
+ * Author   : Bavensky :3                                                      *
+ * E-Mail   : Aphirak_Sang-ngenchai@hotmail.com                                *
+ * Date     : 18/04/2014 [dd/mm/yyyy]                                          *
+ *******************************************************************************/
 int latchPin = 12;
 int clockPin = 13;
 int dataPin = 11;
@@ -14,17 +22,19 @@ pinMode(dataPin, OUTPUT);
 
 void loop() 
 {
-  A = 0xFF; B = 255; Send(B,A); 
   // ฐาน 2  = 0B11111111
   // ฐาน 10 = 255
   // ฐาน 16 = 0xFF
+  
+  A = 0xFF; B = 255; Send(B,A); 
+  
 }
 
-void Send(int X2,int X1)
+void Send(int ic2,int ic1)
 {
   digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST,X2);
-  shiftOut(dataPin, clockPin, MSBFIRST,X1); 
+  shiftOut(dataPin, clockPin, MSBFIRST,ic2);
+  shiftOut(dataPin, clockPin, MSBFIRST,ic1); 
   digitalWrite(latchPin, HIGH);
   delay(1000);
 }
