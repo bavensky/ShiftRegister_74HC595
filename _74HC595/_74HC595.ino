f2856b86a1dcbe160facbe9d6fc0c94c3f22b853
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2014-04-19 18:34:32 +0700

    Bavensky

--- _74HC595/_74HC595.ino
+++ _74HC595/_74HC595.ino
@@ -8,15 +8,14 @@
  * E-Mail   : Aphirak_Sang-ngenchai@hotmail.com                                *
  * Date     : 18/04/2014 [dd/mm/yyyy]                                          *
  *******************************************************************************/
-int latchPin = 12;
-int clockPin = 13;
-int dataPin = 11;
+int latchPin = 12;  // Pin connected to ST_CP 
+int clockPin = 13;  // Pin connected to SH_CP 
+int dataPin = 11;   // Pin connected to DS 
 
-int A = 0;
-int B = 0;
+int A = 0;  int B = 0;
 
 void setup() {
-pinMode(latchPin, OUTPUT);
+pinMode(latchPin, OUTPUT);  // Set pin output
 pinMode(clockPin, OUTPUT);
 pinMode(dataPin, OUTPUT);
 }
@@ -27,15 +26,28 @@ void loop()
   // ฐาน 10 = 255
   // ฐาน 16 = 0xFF
   
-  A = 0xFF; B = 255; Send(B,A); 
+  A = 0; B = 0B00000000; 
+  
+  Send(B,A);
+  
+  // A คือส่งข้อมูลเข้าไอซีตัวแรก
+  // B คือส่งข้อมูลเข้าไอซีตัวที่สอง 
   
 }
 
+/*******************************************************************************
+* MSB = Most significant bit  / LSB = Less significant bit                     *
+* เช่น MSB---> 11000101 <--- LSB                                                *
+* 1 ตัวซ้ายสุดคือ MSB และ 1 ตัวขวาสุดคือ LSB                                         *
+* ถ้ากำหนดว่าส่งข้อมูลแบบ serial โดยส่ง LSB ก่อน                                     *
+* เราก็ต้องจะส่งข้อมูล 1-0-1-0-0-0-1-1 ตามลำดับ                                      *
+*******************************************************************************/
 void Send(int ic2,int ic1)
 {
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST,ic2);
-  shiftOut(dataPin, clockPin, MSBFIRST,ic1); 
-  digitalWrite(latchPin, HIGH);
+  digitalWrite(latchPin, LOW);                
+  shiftOut(dataPin, clockPin, LSBFIRST,ic2);  
+  shiftOut(dataPin, clockPin, MSBFIRST,ic1);  
+  digitalWrite(latchPin, HIGH);     
   delay(1000);
 }
+
